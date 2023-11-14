900451e2bab1d8a0d60f91864f5ce0def90a4b31
kobbin003 <dkfeto@gmail.com>
2023-11-14 13:23:38 +0530

    vercel deploy

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -14,7 +14,7 @@ const authOptions = {
 };
 
 // export default NextAuth(authOptions);
-export const handler = NextAuth(authOptions);
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
 
